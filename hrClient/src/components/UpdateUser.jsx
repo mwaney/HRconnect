f10043ff1d65e45a15d43a8a4ce1d67a28e714ee
mwaney <emwaniki91@gmail.com>
2023-07-13 16:12:35 +0300

    Add Update logic on the update form

--- hrClient/src/components/UpdateUser.jsx
+++ hrClient/src/components/UpdateUser.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function UpdateUser() {
+  const { id } = useParams();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [phone, setPhone] = useState();
+  const [age, setAge] = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5656/api/employees/" + id)
+      .then((result) => {
+        console.log(result);
+        setName(result.data.name);
+        setEmail(result.data.email);
+        setPhone(result.data.phone);
+        setAge(result.data.age);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const Update = (event) => {
+    event.preventDefault();
+
+    axios
+      .put("http://localhost:5656/api/employees/" + id, {
+        name,
+        email,
+        phone,
+        age,
+      })
+      .then((result) => {
+        console.log(result);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
-        <form>
+        <form onSubmit={Update}>
           <h2>Update Employee</h2>
           <div className="mb-2">
             <label htmlFor="">Name</label>
@@ -12,6 +50,8 @@ function UpdateUser() {
               type="text"
               placeholder="Enter Name"
               className="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="mb-2">
@@ -20,6 +60,8 @@ function UpdateUser() {
               type="email"
               placeholder="Enter Email"
               className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-2">
@@ -28,6 +70,8 @@ function UpdateUser() {
               type="tel"
               placeholder="Enter Phone Number"
               className="form-control"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
             />
           </div>
           <div className="mb-2">
@@ -36,6 +80,8 @@ function UpdateUser() {
               type="number"
               placeholder="Enter Age"
               className="form-control"
+              onChange={(e) => setPhone(e.target.value)}
+              value={age}
             />
           </div>
           <button className="btn btn-success">Update</button>
--- hrClient/src/components/Users.jsx
+++ hrClient/src/components/Users.jsx
@@ -36,7 +36,10 @@ function Users() {
                   <td>{user.phone}</td>
                   <td>{user.age}</td>
                   <td>
-                    <Link to="/update" className="btn btn-primary">
+                    <Link
+                      to={`/update/${user._id}`}
+                      className="btn btn-primary"
+                    >
                       Update
                     </Link>
                     <button className="btn btn-danger">Delete</button>
