f17a816e2129c33d94f90a849dea82160a55945d
mwaney <emwaniki91@gmail.com>
2023-07-13 13:25:59 +0300

    Refactor and add Controllers folder

---
+++ hrServer/controllers/employees.js
@@ -0,0 +1,73 @@
+const Employees = require("../models/employees");
+
+const AddEmployee = async (req, res) => {
+  try {
+    const { name, email, phone, age } = req.body;
+    if (!name || !email || !phone || !age) {
+      res.status(400).res.send("All fields are mandatory");
+    }
+    const employee = await Employees.create(req.body);
+    res.status(201).send(employee);
+  } catch (err) {
+    res.status(400).send("Unable to Create the employee");
+    console.log("Unable to Create the employee", err.message);
+  }
+};
+
+const getEmployees = async (req, res) => {
+  try {
+    const employee = await Employees.find();
+    res.status(200).send(employee);
+  } catch (err) {
+    res.status(400).send("Can't get employees");
+    console.log("Can't get employees", err.message);
+  }
+};
+const getEmployee = async (req, res) => {
+  try {
+    const employee = await Employees.findById(req.params.id);
+    if (!employee) return res.status(400).send("Employee doesn't exist");
+
+    res.status(200).send(employee);
+  } catch (error) {
+    res.status(404).send(`Employee with id ${req.params.id} doesn't exist`);
+    console.log("Get Employee Error: ", error.message);
+  }
+};
+const updateEmployee = async (req, res) => {
+  try {
+    const employee = await Employees.findById(req.params.id);
+    if (!employee) return res.status(400).send("Employee doesn't exist");
+
+    const updatedEmployee = await Employees.findByIdAndUpdate(
+      employee._id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).send(updatedEmployee);
+  } catch (error) {
+    res.status(404).send(`Can't Update Employee with id ${req.params.id}`);
+    console.log("PUT Employee Error: ", error.message);
+  }
+};
+const deleteEmployee = async (req, res) => {
+  try {
+    const employee = await Employees.findById(req.params.id);
+    if (!employee) return res.status(400).send("Employee doesn't exist");
+
+    const deletedEmployee = await Employees.deleteOne({ _id: employee._id });
+    res.status(200).send(employee);
+    console.log(deletedEmployee);
+  } catch (error) {
+    res.status(400).send(`Unable to delete employee with id ${req.params.id}`);
+    console.error("Delete Error: ", error.message);
+  }
+};
+
+module.exports = {
+  AddEmployee,
+  getEmployees,
+  getEmployee,
+  updateEmployee,
+  deleteEmployee,
+};
--- hrServer/routes/employee.js
+++ hrServer/routes/employee.js
@@ -1,71 +1,21 @@
 const express = require("express");
-const Employees = require("../models/employees");
+const {
+  AddEmployee,
+  getEmployees,
+  getEmployee,
+  updateEmployee,
+  deleteEmployee,
+} = require("../controllers/employees");
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  try {
-    const { name, email, phone, age } = req.body;
-    if (!name || !email || !phone || !age) {
-      res.status(400).res.send("All fields are mandatory");
-    }
-    const employee = await Employees.create(req.body);
-    res.status(201).send(employee);
-  } catch (err) {
-    res.status(400).send("Unable to Create the employee");
-    console.log("Unable to Create the employee", err.message);
-  }
-});
+router.post("/", AddEmployee);
 
-router.get("/", async (req, res) => {
-  try {
-    const employee = await Employees.find();
-    res.status(200).send(employee);
-  } catch (err) {
-    res.status(400).send("Can't get employees");
-    console.log("Can't get employees", err.message);
-  }
-});
+router.get("/", getEmployees);
 
-router.get("/:id", async (req, res) => {
-  try {
-    const employee = await Employees.findById(req.params.id);
-    if (!employee) return res.status(400).send("Employee doesn't exist");
+router.get("/:id", getEmployee);
 
-    res.status(200).send(employee);
-  } catch (error) {
-    res.status(404).send(`Employee with id ${req.params.id} doesn't exist`);
-    console.log("Get Employee Error: ", error.message);
-  }
-});
+router.put("/:id", updateEmployee);
 
-router.put("/:id", async (req, res) => {
-  try {
-    const employee = await Employees.findById(req.params.id);
-    if (!employee) return res.status(400).send("Employee doesn't exist");
+router.delete("/:id", deleteEmployee);
 
-    const updatedEmployee = await Employees.findByIdAndUpdate(
-      employee._id,
-      req.body,
-      { new: true }
-    );
-    res.status(200).send(updatedEmployee);
-  } catch (error) {
-    res.status(404).send(`Can't Update Employee with id ${req.params.id}`);
-    console.log("PUT Employee Error: ", error.message);
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  try {
-    const employee = await Employees.findById(req.params.id);
-    if (!employee) return res.status(400).send("Employee doesn't exist");
-
-    const deletedEmployee = await Employees.deleteOne({ _id: employee._id });
-    res.status(200).send(employee);
-    console.log(deletedEmployee);
-  } catch (error) {
-    res.status(400).send(`Unable to delete employee with id ${req.params.id}`);
-    console.error("Delete Error: ", error.message);
-  }
-});
 module.exports = router;
