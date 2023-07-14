0a3baa2ef3e1a3a4570d701efe35c00e36c98608
mwaney <emwaniki91@gmail.com>
2023-07-14 15:04:46 +0300

    Change colors

--- hrClient/src/components/Users.jsx
+++ hrClient/src/components/Users.jsx
@@ -33,20 +33,20 @@ function Users() {
       .catch((err) => console.log(err));
   };
   return (
-    <div className="d-flex flex-column vh-100 bg-warning justify-content-center align-items-center">
+    <div className="d-flex flex-column vh-100 bg-primary justify-content-center align-items-center">
       <div className="mb-3">
         <h1
           style={{
             fontWeight: "bold",
             fontFamily: "fantasy",
-            color: "#5B7C99",
+            color: "#FFF",
           }}
         >
           HrConnect Employee DashBoard
         </h1>
       </div>
       <div className="w-70 bg-white rounded p-3">
-        <Link to="/create" className="btn btn-info mb-2">
+        <Link to="/create" className="btn btn-warning mb-2">
           Add +
         </Link>
         <table className="table">
