2e3b04f88fc2738e37249ac9e09a0263a984c6d2
Kelvin Ayonga <kelvin.ayonga@pixeledge.io>
2023-08-03 20:19:44 +0300

    code: Moved App and Login components into separate folder for organization

--- hrClient/src/App/App.jsx
+++ hrClient/src/App/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Users from "./components/Users";
-import CreateUser from "./components/CreateUser";
-import UpdateUser from "./components/UpdateUser";
-import OneUSer from "./components/OneUSer";
-import SignUp from "./components/SignUp";
-import LoginForm from "./components/LogIn";
+import Users from "../components/Users";
+import CreateUser from "../components/CreateUser";
+import UpdateUser from "../components/UpdateUser";
+import OneUSer from "../components/OneUSer";
+import SignUp from "../components/SignUp";
+import Login from "../components/Login";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SignUp />}></Route>
-          <Route path="/login" element={<LoginForm />}></Route>
+          <Route path="/login" element={<Login />}></Route>
           <Route path="/allemployees" element={<Users />}></Route>
           <Route path="/create" element={<CreateUser />}></Route>
           <Route path="/update/:id" element={<UpdateUser />}></Route>
---
+++ hrClient/src/components/Login/Login.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Col, Row, Form, Button, Container } from "react-bootstrap";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import "./Login.css";
+
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .required("Email is required")
+    .matches(
+      /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+      "Invalid Email Address"
+    ),
+  password: Yup.string().required("Please Enter your password"),
+});
+function Login() {
+  const formik = useFormik({
+    initialValues: { email: "", password: "" },
+    validationSchema,
+    onSubmit: (values) => {},
+  });
+  return (
+   <Container>
+    <Row>
+      <Col className="mt-5 pt-5"  xl={{ span: 4, offset: 4 }}
+            lg={{ span: 4, offset: 4 }}
+            md={{ span: 6, offset: 3 }}
+            sm={{ span: 8, offset: 2 }}>
+          <div className="bg-white p-4 rounded shadow-lg">
+            <h2 className="text-center text-primary">Login</h2>
+            <hr />
+            <Form onSubmit={formik.handleSubmit}>
+              <Form.Group className="mb-3" controlId="formEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter your email address"
+                  {...formik.getFieldProps("email")}
+                />
+                {formik.touched.email && formik.errors.email ? (
+                  <div className="text-danger"><small>{formik.errors.email}</small></div>
+                ) : null}
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter your password"
+                  {...formik.getFieldProps("password")}
+                />
+                {formik.touched.password && formik.errors.password ? (
+                  <div className="text-danger"><small>{formik.errors.password}</small></div>
+                ) : null}
+              </Form.Group>
+              
+              <div className="d-grid mb-3">
+                <Button
+                  variant="primary"
+                  type="submit"
+                >
+                  Sign In
+                </Button>
+            </div>
+            </Form>
+            <p className="text-center">
+                <small>Don't Have an Account? <a href="/">Register</a></small>
+            </p>
+            <p className="text-center ">
+              <small><a href="#" className="text-dark">
+                Forgot Password?
+              </a></small>
+            </p>
+          </div>
+      </Col>
+    </Row>
+   </Container>
+  );
+}
+
+export default Login;
